cc733afd6198957355b2c8fd63f4b113204795d9
Nicolas CARREZ <nicolas.carrez@gmail.com>
2018-10-27 16:52:31 +0200

    Partial => res.sendPage

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 var msaMain = module.exports = Msa.module("")
 
 //var msaNavmenu = require("../../msa-navmenu/msa-server")
-var msaUser = Msa.require("user")
+const msaUser = Msa.require("user")
 
-var formatHtml = Msa.formatHtml
+const formatHtml = Msa.formatHtml
 
 // params
 
 Msa.params.default_route = "/page/home"
 
 // template
-var path = require('path'),
+const path = require('path'),
 	join = path.join
-var fs = require('fs')
-var mustache = require('mustache')
-var template = fs.readFileSync(join(__dirname, 'views/index.html'), "utf8")
+const fs = require('fs')
+const { promisify:prm } = require('util')
+const mustache = require('mustache')
+const template = fs.readFileSync(join(__dirname, 'views/index.html'), "utf8")
 mustache.parse(template)
-
-// default route
-msaMain.defaultRoute = Msa.params.default_route
-/*msaMain.setDefaultRoute = function(next) {
-	msaUser.isFirstRegisterDone(function(err, done) {
-		if(err) return next(err)
-		if(done) msaMain.defaultRoute = Msa.params.default_route
-		else {
-			msaMain.defaultRoute = "/user/firstregister"
-			msaUser.onFirstRegister = function(next) {
-				msaMain.setDefaultRoute(next)
-			}
-		}
-		next && next()
-	})
-}
-msaMain.setDefaultRoute(function(err) {
-	if(err) console.log(err)
-})*/
 
 // log
 if(Msa.params.log_level==="DEBUG") {
@@ -52,94 +34,38 @@ msaMain.app.get("/", function(req, res, next) {
 // import some middlewares
 msaMain.app.use(Msa.bodyParser.text())
 msaMain.app.use(Msa.bodyParser.json())
-msaMain.app.use(Msa.bodyParser.urlencoded({extended:false}))
-// msaMain.app.use(Msa.preRouter)
+msaMain.app.use(Msa.bodyParser.urlencoded({ extended:false }))
 
-// partialApp
-//var partialApp = Msa.subApp()
-//msaMain.app.use("/partial", partialApp)
+const msaModsMdw = Msa.modulesRouter
+msaMain.msaModsMdw = function(req, res, next) {
+	res.sendPage = sendPage
+	res._req = req
+	msaModsMdw(req, res, next)
+}
+msaMain.app.use(msaMain.msaModsMdw)
 
-// msaMain app routing
-msaMain.app.use(Msa.modulesRouter)
-//partialApp.use(Msa.subAppsRouter)
-
-var getPartialPrm = msaMain.getPartialPrm = function(getPartial, req, res){
-  return new Promise((resolve, reject) => {
-    getPartial(req, res, () => {
-      if(res.partial === undefined) resolve(null)
-      var partial = formatHtml(res.partial)
-      delete res.partial
-      resolve(partial)
-    })
-  })
+// will be a method of "res"
+const getUserHtml= msaUser.getHtml
+const sendPage = function(htmlExpr) {
+	try {
+		const contentPartial = formatHtml(htmlExpr)
+		// get user partial
+		getUserHtml(this._req, this, userPartial => {
+			_sendPage2({ contentPartial, userPartial }, this)
+		})
+	} catch(err) { this.sendStatus(500); console.warn(err) }
 }
-
-// render view with partial content
-msaMain.get = async function(req, res, next) {
-  try {
-    var contentPartial = res.partial
-    if(contentPartial === undefined) return next()
-//    var headerPartial = await getPartialPrm(msaNavmenu.getPartial, req, res)
-    var userPartial = await getPartialPrm(msaUser.getPartial, req, res)
-    // send content
-    res.setHeader('content-type', 'text/html')
-    var html = mustache.render(template, {
-//      headerPartial: headerPartial,
-      userPartial: userPartial,
-      contentPartial: contentPartial
-    })
-    res.send(html)
-  } catch(err) {
-    next(err)
-  }
+const _sendPage2 = function(partials, res) {
+	try {
+		// send content
+		res.setHeader('content-type', 'text/html')
+		const html = mustache.render(template, partials)
+		res.send(html)
+	} catch(err) { res.sendStatus(500); console.warn(err) }
 }
-/*
-	// check if a sub app have replied a partial
-	if(res.partial===undefined) return next()
-	var contentPartial = formatHtml(res.partial)
-	delete res.partial
-	// get partial from navmenu
-	msaNavmenu.getPartial(req, res, function(){
-		_mainGet2(contentPartial, req, res, next)
-	})
-}
-var _mainGet2 = function(contentPartial, req, res, next) {
-	if(res.partial===undefined) return next("No partial returned from navmenu component.")
-	var headerPartial = formatHtml(res.partial)
-	delete res.partial
-	// get partial form user
-	msaUser.getPartial(req, res, function(){
-		_mainGet3(contentPartial, headerPartial, res, next)
-	})
-}
-var _mainGet3 = function(contentPartial, headerPartial, res, next) {
-	if(res.partial===undefined) return next("No partial returned from user component.")
-	var userPartial = formatHtml(res.partial)
-	delete res.partial
-	// send content
-	res.setHeader('content-type', 'text/html')
-	var html = mustache.render(template, {
-//		headerPartial: headerPartial,
-//		userPartial: userPartial,
-		contentPartial: contentPartial
-	})
-	res.send(html)
-}
-*/
-msaMain.app.get('*', msaMain.get)
-
-// render partial as ajax
-/*
-partialApp.get('*', function(req, res, next) {
-	if(!res.partial) return next()
-	res.json(res.partial)
-})
-*/
-// static routing
-//main.app.use(Msa.express.static(Msa.dirname))
 
 // error handling
-msaMain.app.use(function(err, req, res, next){
+msaMain.handleErrMdw = function(err, req, res, next){
 	// determine error code & text
 	if(typeof err=='object') {
 		if(err instanceof Error) var text=err
@@ -155,4 +81,5 @@ msaMain.app.use(function(err, req, res, next){
 	if(Msa.params.log_level==="DEBUG") {
 		console.error('ERROR', code, text)
 	}
-})
+}
+msaMain.app.use(msaMain.handleErrMdw)
