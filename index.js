15834f3a3b8e8523d867f7143e6b7a7b732f94bb
Nicoco220983 <nicolas.carrez@gmail.com>
2019-03-10 21:19:40 +0100

    process PID in DEBUG log

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const MsaApp = class extends Msa.Module {
 	initDebugMdw(){
 		if(Msa.params.log_level==="DEBUG") {
 			this.app.use((req, res, next) => {
-				console.log(req.method+' '+req.url)
+				console.log(`[${process.pid}] ${req.method} ${req.url}`)
 				next()
 			})
 		}
