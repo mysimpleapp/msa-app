9814ebff503aa1da8c6041cc020d985a96edc1f0
Nicolas CARREZ <nicolas.carrez@clustree.com>
2020-02-09 14:31:16 +0100

    formatting + fix default route

--- module.js
+++ module.js
@@ -27,20 +27,20 @@ const MsaApp = class extends Msa.Module {
 	}
 
 	initApp() {
-		if(this.initFirstMdws) this.initFirstMdws()
-		if(this.initDebugMdw) this.initDebugMdw()
-		if(this.initDefaultRouteMdw) this.initDefaultRouteMdw()
-		if(this.initMsaModsMdw) this.initMsaModsMdw()
-		if(this.initEndMdws) this.initEndMdws()
-		if(this.initStaticMdw) this.initStaticMdw()
-		if(this.initErrMdw) this.initErrMdw()
+		if (this.initFirstMdws) this.initFirstMdws()
+		if (this.initDebugMdw) this.initDebugMdw()
+		if (this.initDefaultRouteMdw) this.initDefaultRouteMdw()
+		if (this.initMsaModsMdw) this.initMsaModsMdw()
+		if (this.initEndMdws) this.initEndMdws()
+		if (this.initStaticMdw) this.initStaticMdw()
+		if (this.initErrMdw) this.initErrMdw()
 	}
 
 	// first middlewares
 	initFirstMdws() {
 		this.app
 			.use(Msa.express.json())
-			.use(Msa.express.urlencoded({ extended:false }))
+			.use(Msa.express.urlencoded({ extended: false }))
 			.use((req, res, next) => {
 				res.sendPage = this.sendPage
 				res._req = req
@@ -50,11 +50,11 @@ const MsaApp = class extends Msa.Module {
 	}
 
 	// debug log
-	initDebugMdw(){
-		if(Msa.params.log_level==="DEBUG") {
+	initDebugMdw() {
+		if (Msa.params.log_level === "DEBUG") {
 			this.app.use((req, res, next) => {
 				let msg = [`[${process.pid}]`, req.method, req.url]
-				if(req.body && !isEmptyObj(req.body)) msg.push(req.body)
+				if (req.body && !isEmptyObj(req.body)) msg.push(req.body)
 				console.log(...msg)
 				next()
 			})
@@ -63,8 +63,8 @@ const MsaApp = class extends Msa.Module {
 
 	// default route
 	initDefaultRouteMdw() {
-		const defRoute = globalParams.app.default_route
-		if(defRoute) {
+		const defRoute = globalParams.app.default_route.get()
+		if (defRoute) {
 			this.app.get("/", (req, res, next) => {
 				res.redirect(defRoute)
 			})
@@ -82,21 +82,21 @@ const MsaApp = class extends Msa.Module {
 	}
 
 	// error handling
-	initErrMdw(){
+	initErrMdw() {
 		this.app.use((err, req, res, next) => {
 			var text, code
 			// determine error code & text
-			if(typeof err=='object') {
-				if(err instanceof Error) text=err
-				else code=err.code, text=err.text
-			} else if(typeof err=='number') code=err
-			else text=err
-			if(!code) code=500
+			if (typeof err == 'object') {
+				if (err instanceof Error) text = err
+				else code = err.code, text = err.text
+			} else if (typeof err == 'number') code = err
+			else text = err
+			if (!code) code = 500
 			// respond to client
-			if(!text || code>=500) res.sendStatus(code)
+			if (!text || code >= 500) res.sendStatus(code)
 			else res.status(code).send(text)
 			// log error
-			if(Msa.params.log_level==="DEBUG")
+			if (Msa.params.log_level === "DEBUG")
 				console.error('ERROR', code, text || '')
 		})
 	}
@@ -106,34 +106,34 @@ const MsaAppPt = MsaApp.prototype
 // sendPage
 // (will be a method of "res")
 const msaUser = Msa.require("user")
-const getUserHtml= msaUser.getHtml
-MsaAppPt.sendPage = function(htmlExpr) {
+const getUserHtml = msaUser.getHtml
+MsaAppPt.sendPage = function (htmlExpr) {
 	try {
 		const contentPartial = formatHtml(htmlExpr)
 		// get user partial
 		getUserHtml(this._req, this, userPartial => {
 			_sendPage2({ contentPartial, userPartial }, this)
 		})
-	} catch(err) { this.sendStatus(500); console.warn(err) }
+	} catch (err) { this.sendStatus(500); console.warn(err) }
 }
-const _sendPage2 = function(partials, res) {
+const _sendPage2 = function (partials, res) {
 	try {
 		// send content
 		res.setHeader('content-type', 'text/html')
 		const html = res._msaApp.renderTemplate(partials)
 		res.send(html)
-	} catch(err) { res.sendStatus(500); console.warn(err) }
+	} catch (err) { res.sendStatus(500); console.warn(err) }
 }
 
 // utils
 const path = require('path'),
 	join = path.join
 const fs = require('fs')
-const { promisify:prm } = require('util')
+const { promisify: prm } = require('util')
 const mustache = require('mustache')
 const readFile = prm(fs.readFile)
 
-function isEmptyObj(o){
+function isEmptyObj(o) {
 	return (typeof o === "object") && (Object.keys(o).length === 0)
 }
 
